36b6cae3b5cf543a50555de3c447d3935daa2b62
elpekenin <elpekenin@elpekenin.dev>
2024-03-15 07:40:43 +0100

    Simplify logic

--- src/layer_callback.c
+++ src/layer_callback.c
@@ -19,40 +19,43 @@ LOG_MODULE_DECLARE(elpekenin, CONFIG_ZMK_LOG_LEVEL);
 
 #include <drivers/behavior.h>
 
+// TODO: Do not hardcode on/off and/or make the yaml check for those names
 
 #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
-// TODO: Make array flexible
-//       Probably needs iterating all the nodes and creating standalone ...
-//       ... structs and then reference (&) them
-struct action {
-    uint8_t count;
-    struct zmk_behavior_binding bindings[30];
-};
-
 struct layer_cb_cfg {
     int8_t layer;
-    struct action on;
-    struct action off;
+    size_t on_count;
+    size_t off_count;
+    struct zmk_behavior_binding bindings[];
 };
 
-#define EXTRACT(n) \
-    {LISTIFY(DT_PROP_LEN(n, bindings), ZMK_KEYMAP_EXTRACT_BINDING, (, ), n)}
+#define ON_NODE(n) DT_CHILD(n, on)
+#define OFF_NODE(n) DT_CHILD(n, off)
+
+#define _EXTRACT_BINDINGS(n) \
+    LISTIFY(DT_PROP_LEN(n, bindings), ZMK_KEYMAP_EXTRACT_BINDING, (, ), n)
 
 #define EXTRACT_BINDINGS(n) \
     { \
-        .count = DT_PROP_LEN(n, bindings), \
-        .bindings = EXTRACT(n) \
+        _EXTRACT_BINDINGS(ON_NODE(n)), \
+        _EXTRACT_BINDINGS(OFF_NODE(n)) \
     }
 
-#define LAYER_CB_INST_AND_COMMA(n) \
-    { \
+#define LAYER_CB_STRUCT(n) \
+    static struct layer_cb_cfg callback_##n = { \
         .layer = DT_PROP(n, layer), \
-        .on = EXTRACT_BINDINGS(DT_CHILD(n, on)), \
-        .off = EXTRACT_BINDINGS(DT_CHILD(n, off)) \
-    },
+        .on_count = DT_PROP_LEN(ON_NODE(n), bindings), \
+        .off_count = DT_PROP_LEN(OFF_NODE(n), bindings), \
+        .bindings = EXTRACT_BINDINGS(n) \
+    };
 
-static struct layer_cb_cfg callbacks[] = {
-    DT_INST_FOREACH_CHILD(0, LAYER_CB_INST_AND_COMMA)
+DT_INST_FOREACH_CHILD(0, LAYER_CB_STRUCT)
+
+#define LAYER_CB_STRUCT_REF_AND_COMMA(n) \
+    &callback_##n
+
+static struct layer_cb_cfg *callbacks[] = {
+    DT_INST_FOREACH_CHILD(0, LAYER_CB_STRUCT_REF_AND_COMMA)
 };
 
 #define N_CBS ARRAY_SIZE(callbacks)
@@ -64,28 +67,24 @@ static struct layer_cb_cfg callbacks[] = {
 static int layer_change_listener(const zmk_event_t *eh) {
     struct zmk_layer_state_changed *evt = as_zmk_layer_state_changed(eh);
 
-    for (
-        const struct layer_cb_cfg *callback = callbacks;
-        callback < &callbacks[N_CBS];
-        ++callback
-    ) {
+    for (size_t i = 0; i < N_CBS; ++i) {
+        const struct layer_cb_cfg *callback = callbacks[i];
+
         if (
             evt->layer != callback->layer
-            || (evt->state && !callback->on.count)
-            || (!evt->state && !callback->off.count)
+            || (evt->state && !callback->on_count)
+            || (!evt->state && !callback->off_count)
         ) {
             continue;
         }
 
-        LOG_DBG("Running for layer %d: %d", evt->layer, evt->state);
-        struct action action = (evt->state ? callback->on : callback->off);
-        for (
-            const struct zmk_behavior_binding *binding = action.bindings;
-            binding < &action.bindings[action.count];
-            binding++
-        ) {
-            zmk_behavior_queue_add(0, *binding, true, 0);  // press
-            zmk_behavior_queue_add(0, *binding, false, 0); // release
+        size_t offset = (evt->state ? 0 : callback->on_count);
+        size_t count  = (evt->state ? callback->on_count : callback->off_count);
+        for (size_t i = offset; i < (offset + count); ++i) {
+            const struct zmk_behavior_binding binding = callback->bindings[i];
+
+            zmk_behavior_queue_add(0, binding, true, 0);  // press
+            zmk_behavior_queue_add(0, binding, false, 0); // release
         }
     }
 
